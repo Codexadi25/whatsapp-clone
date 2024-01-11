7f3361149f03b442ecaa2225c418c7599b1359cf
Codexadi25 <111916672+Codexadi25@users.noreply.github.com>
2024-01-11 19:14:43 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Chat.css 	new file:   src/Chat.js 	new file:   src/Sidebar.css 	new file:   src/Sidebar.js 	modified:   src/index.css

---
+++ src/Sidebar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Sidebar.css';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { Avatar, IconButton } from '@mui/material';
+
+
+
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar__header'>
+         <Avatar/>
+         <div className='sidebar__headerRight'>
+            <IconButton>
+               <DonutLargeIcon/>
+            </IconButton>
+            <IconButton>
+               <ChatIcon/>
+            </IconButton>
+            <IconButton>
+               <MoreVertIcon/>
+            </IconButton>
+         </div>
+      </div>
+      <div className='sidebar__search'>
+         <div className='sidebar__searchContainer'>
+            <SearchIcon/>
+            <input type='text' placeholder='Search here to start chat.'/>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
